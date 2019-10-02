24e6de44409e800298bf70378cbfead763dabbc8
Anwesh43 <anweshthecool0@gmail.com>
2019-10-02 12:57:10 +0530

    adding DrawingUtil methods

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ const backColor : string = "#BDBDBD"
 const delay : number = 20
 const parts : number = 4
 const nodes : number = 5
+const sizeFactor : number = 90
+const rFactor : number = 3
 
 class Stage {
 
@@ -47,5 +49,51 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.save()
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+        context.restore()
+    }
+
+    static drawLineToCircle(context : CanvasRenderingContext2D, i : number, size : number, scale : number) {
+        const sci : number = ScaleUtil.divideScale(scale, i, parts)
+        context.save()
+        context.rotate(i * Math.PI / 2)
+        DrawingUtil.drawLine(context, -size + 2 * size * sci, -size, size, -size)
+        DrawingUtil.drawCircle(context, size, -size, (size / rFactor) * sci)
+        context.restore()
+    }
+
+    static drawLineToCircles(context : CanvasRenderingContext2D, size : number, scale : number) {
+        for (var i = 0; i < parts; i++) {
+            DrawingUtil.drawLineToCircle(context, i, size, scale)
+        }
+    }
+
+    static drawLSBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const gap : number = w / (nodes + 1)
+        const size : number = gap / sizeFactor
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = foreColor
+        context.fillStyle = foreColor
+        context.save()
+        context.translate(gap * (i + 1), h / 2)
+        DrawingUtil.drawLineToCircles(context, size, scale)
+        context.restore()
     }
 }
